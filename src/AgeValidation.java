9a144dfc164ebba78d098152b1bcc3759b679561
SCIS3-PC07\SCIS3-07 <colombia@hotmail.com>
2024-09-17 07:22:23 -0500

    se pudo

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -4,12 +4,13 @@ public class AgeValidation {
         //creamos las constantes
         final int AGE_LIMIT = 18;
         System.out.println("¿Cual es tu edad?");
-        Scanner scan = new Scanner(System.in);
-        int age = scan.nextInt();
-        if ( age >= AGE_LIMIT) {
-            System.out.println("Access granted");
-        } else{
-            System.out.println("Access denied");
+        try (Scanner escribir = new Scanner(System.in)) {
+            int age = escribir.nextInt();
+            if ( age >= AGE_LIMIT) {
+                System.out.println("Access granted");
+            } else{
+                System.out.println("Access denied");
+            }
         }
         
        
